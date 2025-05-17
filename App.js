18621b9cba09e46e8589324e81c3b9a9a3d7d1d5
Gabriel <gdamaceno2002@gmail.com>
2025-05-17 11:41:34 -0300

    sorteio de jogos

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MegaSena from './screens/MegaSena';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons'
+import JogoBicho from './screens/JogoBicho';
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen 
+          name='MegaSena'
+          component={MegaSena}
+          options={{
+            title: 'Sorteio Mega Sena',
+            tabBarIcon: ({color,size})=> <Ionicons name='person' color={color} size={size} />
+          }}
+          />
+
+          <Tab.Screen 
+          name='JogoBicho'
+          component={JogoBicho}
+          options={{
+            title: 'Jogo do Bicho',
+            tabBarIcon: ({color,size})=> <Ionicons name='person' color={color} size={size} />
+          }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
---
+++ screens/MegaSena.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import { Button, Card, Text } from 'react-native-paper';
+
+export default function MegaSena() {
+  const [numeros, setNumeros] = useState([]);
+
+  function gerar() {
+    const listaNumeros = [];
+    while (listaNumeros.length < 6) {
+      const numeroAleatorio = Math.floor(Math.random() * 60) + 1;
+      listaNumeros.push(numeroAleatorio);
+    }
+    setNumeros(listaNumeros);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>Gerador da Mega Sena</Text>
+
+      <View style={styles.bolinhasContainer}>
+        {numeros.map((item) => (
+          <Card style={styles.bolinha}>
+            <Card.Content>
+              <Text style={styles.numero}>{item}</Text>
+            </Card.Content>
+          </Card>
+        ))}
+      </View>
+
+      <Button mode="contained" onPress={gerar} style={styles.botao}>
+        Gerar números
+      </Button>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 24,
+    textAlign: 'center',
+  },
+  bolinhasContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginBottom: 24,
+  },
+  bolinha: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#26a69a',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 4,
+    elevation: 3,
+  },
+  numero: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  botao: {
+    marginTop: 16,
+  },
+});
